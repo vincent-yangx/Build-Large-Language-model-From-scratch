63cbd237f3de73616cdbbff21ebfdf469d1209ac
vincent-yangx <hamsteryang720@gmail.com>
2024-12-21 03:23:57 -0500

    3.4 finished, question with P91, how to assign weight in V2 to V1

--- chapter3.py
+++ chapter3.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 '''
 3.3 Attending to different parts of the input with self-attention
 '''
@@ -10,6 +11,11 @@ inputs = torch.tensor(
 [0.77, 0.25, 0.10], # one (x^5)
 [0.05, 0.80, 0.55]] # step (x^6)
 )
+'''
+3.3 code
+'''
+
+'''
 
 # calculate the intermediate attention scores
 query = inputs[1] #A
@@ -53,3 +59,90 @@ print(attn_weights)
 
 all_context_vecs = attn_weights @ inputs
 print(all_context_vecs)
+'''
+
+'''
+3.4 Implementing self-attention with trainable weights
+'''
+x_2 = inputs[1] #A
+d_in = inputs.shape[1] #B
+d_out = 2 #C
+
+torch.manual_seed(123)
+W_query = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+W_key = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+W_value = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+
+query_2 = x_2 @ W_query
+key_2 = x_2 @ W_key
+value_2 = x_2 @ W_value
+print(query_2)
+
+keys = inputs @ W_key
+values = inputs @ W_value
+print("keys.shape:", keys.shape)
+print("values.shape:", values.shape)
+
+keys_2 = keys[1] #A
+attn_score_22 = query_2.dot(keys_2)
+print(attn_score_22)
+
+attn_scores_2 = query_2 @ keys.T # All attention scores for given query
+print(attn_scores_2)
+
+# When the dimension of embeddings begins to increase, attention score of some keys will be very big, while
+# others are small, which leads to disappearance of some points. This is why we need √dk here.
+# The reason for the normalization by the embedding dimension size is to
+# improve the training performance by avoiding small gradients.
+d_k = keys.shape[-1]
+attn_weights_2 = torch.softmax(attn_scores_2 / d_k**0.5, dim=-1)
+print(attn_weights_2)
+
+# Need to pay attention to the @ operation
+context_vec_2 = attn_weights_2 @ values
+print(context_vec_2)
+
+# Part 3.4.2
+class SelfAttention_v1(nn.Module):
+    def __init__(self, d_in, d_out):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_key = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+    def forward(self, x):
+        keys = x @ self.W_key
+        queries = x @ self.W_query
+        values = x @ self.W_value
+        attn_scores = queries @ keys.T # omega
+        attn_weights = torch.softmax(
+        attn_scores / keys.shape[-1]**0.5, dim=-1)
+        context_vec = attn_weights @ values
+        return context_vec
+
+torch.manual_seed(123)
+sa_v1 = SelfAttention_v1(d_in, d_out)
+print(sa_v1(inputs))
+
+# replace nn.Parameter with nn.Linear (has an optimized weight initialization scheme Kaiming /Xavier)
+class SelfAttention_v2(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=1)
+        context_vec = attn_weights @ values
+        return context_vec
+
+# From the results we can find out that the sa_v2 is different from sa_v1, difference mainly comes
+# from the weight initialization
+torch.manual_seed(789)
+sa_v2 = SelfAttention_v2(d_in, d_out)
+print(sa_v2(inputs))
